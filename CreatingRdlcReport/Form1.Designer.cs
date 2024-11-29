06a7016547bdfead1ac8407e833fd0c050eec4b4
silvansridhar <silvansridhar@gmail.com>
2024-11-29 17:11:03 +0530

    Uploading RDLC form

---
+++ CreatingRdlcReport/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace CreatingRdlcReport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(223, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(379, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Info Report Using RDLC ";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(335, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load Report";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.reportViewer1.Location = new System.Drawing.Point(2, 90);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(853, 374);
+            this.reportViewer1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(520, 50);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(182, 34);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(858, 476);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.reportViewer1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Create RDLC Report";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ CreatingRdlcReport/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Microsoft.Reporting.WinForms;
+
+namespace CreatingRdlcReport
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+            this.reportViewer1.RefreshReport();
+        }
+
+        SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Sridhar R\Forms\CreatingRdlcReport\CreatingRdlcReport\RdlcReport.mdf;Integrated Security=True");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql = "Select * from UserInfo";
+            SqlCommand cmd = new SqlCommand(sql, Con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            reportViewer1.LocalReport.DataSources.Clear();
+            ReportDataSource Rpt = new ReportDataSource("DataSet1",dt);
+            //reportViewer1.LocalReport.ReportPath = "D:\Sridhar R\Forms\CreatingRdlcReport\CreatingRdlcReport\Report1.rdlc";
+            reportViewer1.LocalReport.ReportPath = "Report1.rdlc";
+            reportViewer1.LocalReport.DataSources.Add(Rpt);
+            reportViewer1.RefreshReport();
+        }
+
+        private void LoadData()
+        {
+            string sql = "Select UserId,UserName,UserLName,,Convert(varchar(10),DOB,101) As DOB,Age,Gender,Address from UserInfo where IsDeleted=0";
+
+            if (Con.State != ConnectionState.Open)
+                Con.Open();
+            SqlCommand cmd = new SqlCommand(sql, Con);
+            SqlDataAdapter lda = new SqlDataAdapter(cmd);
+            DataTable Ldt = new DataTable();
+            lda.Fill(Ldt);
+
+            reportViewer1.LocalReport.DataSources.Clear();
+            ReportDataSource Rpt1 = new ReportDataSource("DataSet1",Ldt);
+            reportViewer1.LocalReport.DataSources.Add(Rpt1);
+            reportViewer1.RefreshReport();
+        }
+
+        private void FilterData()
+        {
+            string sql = "Select UserId,UserName,UserLName,Convert(varchar(10),DOB,101) As DOB,Age,Gender,Address  from UserInfo where IsDeleted=0 AND (UserId Like @Searchdata or UserName Like @Searchdata)";
+
+            if (Con.State != ConnectionState.Open)
+                Con.Open();
+            SqlCommand cmd1 = new SqlCommand(sql, Con);
+            cmd1.Parameters.AddWithValue("@Searchdata","%" + textBox1.Text +"%");
+            SqlDataAdapter fda = new SqlDataAdapter(cmd1);
+            DataTable fdt = new DataTable();
+            fda.Fill(fdt);
+
+            if (fdt.Rows.Count == 0)
+            {
+                MessageBox.Show("No matching records found.", "Search Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+            reportViewer1.LocalReport.DataSources.Clear();
+            ReportDataSource Rpt2 = new ReportDataSource("DataSet1", fdt);
+            reportViewer1.LocalReport.DataSources.Add(Rpt2);
+            reportViewer1.RefreshReport();
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                LoadData();
+            }
+            else
+            {
+                FilterData();
+            }
+        }
+    }
+}
